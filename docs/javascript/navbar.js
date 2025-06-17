3043f460d86fdc5f31047eea2a36ba24ef924340
Muhammad Talha <talha18513@gmail.com>
2025-06-17 07:15:57 +0500

    homr page completed

---
+++ docs/javascript/navbar.js
@@ -0,0 +1,14 @@
+
+const mobileMenuBtn = document.getElementById("mobileMenuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+const mobileMenuCloseBtns = document.querySelectorAll(".mobileMenuCloseBtn");
+
+mobileMenuBtn.addEventListener("click", () => {
+    mobileMenu.style.width = "100%";
+});
+
+mobileMenuCloseBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        mobileMenu.style.width = "0";
+    });
+});
